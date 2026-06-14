89aa222e5eb44580087e6e52708e43bc4d83924e
SetHMode <prueba>
2026-06-14 06:42:20 -0400

    feat: responsive tabla y formulario, icono sitio

--- src/components/historial/TablaBol.tsx
+++ src/components/historial/TablaBol.tsx
@@ -15,40 +15,39 @@ export const TablaBol = ({
   eliminar,
 }: Props) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Boleta</th>
-          <th>Placa</th>
-          <th>Conductor</th>
-          <th>Tipo</th>
-          <th>Marca</th>
-          <th>Artículo</th>
-          <th>Fecha</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {boletas.map((b) => (
-          <tr key={b.nro_boleta}>
-            <td>{b.nro_boleta}</td>
-            <td>{b.placa}</td>
-            <td>{valor(b.nom_conductor)}</td>
-            <td>{b.tip_vehi}</td>
-            <td>{b.marca}</td>
-            <td>
-              {b.art} - {b.nro_infr}
-            </td>
-            <td>{fecha(b.fecha)}</td>
-            <td>
-              <button onClick={() => setSel(b)}>Ver</button>
-              <button onClick={() => editarBoleta(b)}>Editar</button>
-              <button onClick={() => eliminar(b.nro_boleta)}>Eliminar</button>
-            </td>
+    <div className="tablaWrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>Boleta</th>
+            <th>Placa</th>
+            <th>Conductor</th>
+            <th>Tipo</th>
+            <th>Marca</th>
+            <th>Artículo</th>
+            <th>Fecha</th>
+            <th>Acciones</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {boletas.map((b) => (
+            <tr key={b.nro_boleta}>
+              <td>{b.nro_boleta}</td>
+              <td>{b.placa}</td>
+              <td>{valor(b.nom_conductor)}</td>
+              <td>{b.tip_vehi}</td>
+              <td>{b.marca}</td>
+              <td>{b.art} - {b.nro_infr}</td>
+              <td>{fecha(b.fecha)}</td>
+              <td>
+                <button onClick={() => setSel(b)}>Ver</button>
+                <button onClick={() => editarBoleta(b)}>Editar</button>
+                <button onClick={() => eliminar(b.nro_boleta)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
